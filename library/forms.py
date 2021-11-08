52c7941b80e2003da5883e6f74f78fbd27a4b8f7
hadi hammoud <hadihamoud@outlook.com>
2021-11-08 15:54:41 +0200

    added email

--- library/forms.py
+++ library/forms.py
@@ -20,7 +20,7 @@ class AdminSigupForm(forms.ModelForm):
 class StudentUserForm(forms.ModelForm):
     class Meta:
         model=User
-        fields=['first_name','last_name','username','password']
+        fields=['first_name','last_name','username','password','Email']
 
 class StudentExtraForm(forms.ModelForm):
     class Meta:
@@ -35,4 +35,4 @@ class IssuedBookForm(forms.Form):
     #to_field_name value will be stored when form is submitted.....__str__ method of book model will be shown there in html
     isbn2=forms.ModelChoiceField(queryset=models.Book.objects.all(),empty_label="Name and isbn", to_field_name="isbn",label='Name and Isbn')
     enrollment2=forms.ModelChoiceField(queryset=models.StudentExtra.objects.all(),empty_label="Name and enrollment",to_field_name='enrollment',label='Name and enrollment')
-    
+
